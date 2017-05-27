f79d6b9d35928c635539a11dfdd5f98f6dd61400
Loi Tran <lvt15@my.fsu.edu>
2017-05-27 18:34:54 +0700

    forgot to adjust message for routes spec

--- spec/routes/routes_spec.rb
+++ spec/routes/routes_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "routing", :type => :routing do
-    it "routes /menu to users#new" do
+    it "routes /menu to menu page" do
       expect(:get => "/menu").to route_to(
       :controller => "home",
       :action => "menu"
       )
   end
 
-  it "routes /menu to users#new" do
+  it "routes /food_items to index" do
     expect(:get => "/food_items").to route_to(
     :controller => "food_items",
     :action => "index"
