c3d5e5adbd524702940ee8fbb42aee82542b215d
Loi Tran <lvt15@my.fsu.edu>
2017-06-27 10:27:31 +0700

    Remembering order in the session

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -15,6 +15,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
+    redirect_back(fallback_location: order_path)
   end
 
   def destroy
@@ -22,6 +23,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
     @order_items = @order.order_items
+    redirect_back(fallback_location: order_path)
   end
 
   private
